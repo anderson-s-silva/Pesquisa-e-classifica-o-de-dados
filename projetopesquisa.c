93bb56178d81a80cba53700a4644d8b464a03b19
Anderson S. Silva <andsoares2001@gmail.com>
2021-04-26 20:07:09 -0300

    em desenvolvimento (pesquisa e classificação)

---
+++ projetopesquisa.c
@@ -0,0 +1,98 @@
+/**
+* @file cadastro.c
+* @author Mariana Goncalves Rodrigues, Anderson Soares da Silva
+* @r.a: 2151014 e 2150875
+* @date 21 Abr 2021
+* @brief  Cadastro Indexado Sr. Apolonio
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+  int id;
+  char nome[26];
+  char rua[26];
+  char cidade[21];
+  char estado[3];
+}arqdados;
+
+typedef struct{
+  int id;
+  int rrn;
+}indp;
+
+typedef struct{
+  char nome[26];
+  int rrn;
+}ind_sforte;
+
+typedef struct{
+  int id;
+  char cidade[21];
+}ind_sfraco;
+
+int ordenaid(const void * a, const void * b){
+  int r = strcmp((*(indp*)a).id, (*(indp*)b).id);
+
+  if(r == 0){
+    return 0;
+  }else{
+    if(r<0){
+      return -1;
+    }else{
+      return 1;
+    }
+  }
+}
+
+void conteudo(FILE* entrada, arqdados aux, indp* prim, ind_sforte *forte, ind_sfraco *fraco){
+
+  int i=0;
+
+  while(fscanf(entrada, "%3d %25[^\n] %25[^\n] %20[^\n] %2[^\n]", &aux.id, aux.nome, aux.rua, aux.cidade, aux.estado) != EOF){
+    prim[i].id = aux.id;
+    fraco[i].id = aux.id;
+    strncpy(forte[i].nome, aux.nome, 26);
+    strncpy(fraco[i].cidade, aux.cidade, 21);
+    i++;
+  }//end while
+  qsort(prim, 500, sizeof(indp), ordenaid);//ordenacao do id no indice primario por quicksort
+  qsort(fraco, 500, sizeof(ind_sfraco), ordenaid);//ordenacao do id no indice secundario por quicksort
+  for(int c = 0; c < 500; c++){
+    printf("%d\n", prim[c].id);
+  }
+}
+
+void arqdadosrimario(FILE* primario, indp* prim){
+
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  FILE* entrada; //ponteiro para arquivo que vou usar como entrada
+  FILE* primario; //ponteiro para arquivo do indice primario
+  FILE* secundarioforte; //ponteiro para arquivo do indice secundario forte
+  FILE* secundariofraco; //ponteiro para arquivo do indice secundario fraco
+
+  arqdados aux;
+  indp prim[500];
+  ind_sfraco fraco[500];
+  ind_sforte forte[500];
+
+  entrada = fopen("cadastro.txt", "r");
+  primario = fopen("primario.txt", "w");
+  secundarioforte = fopen("secforte.txt", "w");
+  secundariofraco = fopen("secfraco.txt", "w");
+
+  if(entrada == NULL){
+    printf("erro ao ler arquivo");
+  }else{
+    conteudo(entrada, aux, &prim, &forte, &fraco);
+    arqdadosrimario(primario, &prim);
+    // cidadenome(entrada, primario, secundarioforte, secundariofraco, aux);
+  }//else
+  return 0;
+}//main
